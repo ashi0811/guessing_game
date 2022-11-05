442ef4ea5ad07441a48f193ae39b7c996e9e41e9
Ashi0811 <gupta.ashi.mrt08@gmail.com>
2022-11-06 00:08:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const luckyNumber = Math.floor(Math.random() * 20);
+
+console.log("Lucky Number= "+luckyNumber);
+
+const userInput = document.getElementById('guessing');
+const message = document.getElementById('message');
+const checkBtn = document.getElementById('check');
+const main = document.querySelector('main');
+
+function CheckNum(){
+    const userGuess= Number(userInput.value);
+
+    if(userGuess === luckyNumber){
+        message.innerHTML="you guessed it right!!! 🎉🎉";
+        main.style.backgroundColor = 'green';
+    } else {
+        main.style.backgroundColor = 'red';
+        if(userGuess > luckyNumber){
+            message.textContent="your guess is higher 😢";
+        } else{
+            message.textContent="your guess is lower 😢";
+        }
+    }
+}
+
+checkBtn.addEventListener('click', CheckNum);
+
